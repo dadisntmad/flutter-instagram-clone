1b722757ca52e2f41a6e948f0d09d96f9fd71ba2
Ruslan <ruslanlantsov1997@gmail.com>
2023-03-20 12:41:44 +0200

    displaying user data

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram/constants.dart';
 import 'package:instagram/widgets/custom_button.dart';
@@ -8,109 +9,143 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-        title: const Text(
-          'username',
-          style: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.menu,
-              color: Colors.black,
-            ),
-          ),
-        ],
-      ),
-      body: CustomScrollView(
-        slivers: [
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.all(14.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      const ProfileImage(size: 65),
-                      Expanded(
-                        child: Row(
-                          mainAxisSize: MainAxisSize.max,
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            userStatistics(17, 'Posts'),
-                            userStatistics(7, 'Followers'),
-                            userStatistics(17, 'Following'),
-                          ],
-                        ),
-                      ),
-                    ],
+    return StreamBuilder(
+      stream: db.collection('users').doc(auth.currentUser!.uid).snapshots(),
+      builder: (BuildContext context, AsyncSnapshot userSnapshot) {
+        return StreamBuilder(
+          stream: db
+              .collection('posts')
+              .where('uid', isEqualTo: auth.currentUser!.uid)
+              .snapshots(),
+          builder: (BuildContext context,
+              AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> postSnapshot) {
+            final user = userSnapshot.data!;
+            return Scaffold(
+              appBar: AppBar(
+                elevation: 0,
+                backgroundColor: Colors.white,
+                title: Text(
+                  '${user['username']}',
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
                   ),
-                  const SizedBox(height: 14),
-                  const Text(
-                    'full name',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
+                ),
+                actions: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.menu,
+                      color: Colors.black,
                     ),
-                  ),
-                  const SizedBox(height: 14),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: CustomButton(
-                          width: 100,
-                          height: 30,
-                          label: 'Edit Profile',
-                          color: accentGrey,
-                          textColor: Colors.black,
-                          isLoading: false,
-                          onTap: () {},
-                        ),
-                      ),
-                      const SizedBox(width: 4),
-                      Expanded(
-                        child: CustomButton(
-                          width: 100,
-                          height: 30,
-                          label: 'Sign Out',
-                          color: accentGrey,
-                          textColor: Colors.black,
-                          isLoading: false,
-                          onTap: () {},
-                        ),
-                      ),
-                    ],
                   ),
                 ],
               ),
-            ),
-          ),
-          SliverGrid(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              mainAxisSpacing: 1.0,
-              crossAxisSpacing: 1.0,
-              crossAxisCount: 3,
-            ),
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) {
-                return Container(
-                  alignment: Alignment.center,
-                  color: Colors.teal[100 * (index % 9)],
-                  child: Text('grid item $index'),
-                );
-              },
-              childCount: 20,
-            ),
-          ),
-        ],
-      ),
+              body: CustomScrollView(
+                slivers: [
+                  SliverToBoxAdapter(
+                    child: Padding(
+                      padding: const EdgeInsets.all(14.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              const ProfileImage(size: 65),
+                              Expanded(
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.max,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    userStatistics(
+                                      postSnapshot.data!.docs.length,
+                                      postSnapshot.data!.docs.length == 1
+                                          ? 'Post'
+                                          : 'Posts',
+                                    ),
+                                    userStatistics(
+                                      user['followers'].length,
+                                      user['following'].length == 1
+                                          ? 'Follower'
+                                          : 'Followers',
+                                    ),
+                                    userStatistics(
+                                      user['following'].length,
+                                      'Following',
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 14),
+                          if (user['fullName'] != '')
+                            Text(
+                              user['fullName'],
+                              style: const TextStyle(
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          const SizedBox(height: 14),
+                          Row(
+                            children: [
+                              Expanded(
+                                child: CustomButton(
+                                  width: 100,
+                                  height: 30,
+                                  label: 'Edit Profile',
+                                  color: accentGrey,
+                                  textColor: Colors.black,
+                                  isLoading: false,
+                                  onTap: () {},
+                                ),
+                              ),
+                              const SizedBox(width: 4),
+                              Expanded(
+                                child: CustomButton(
+                                  width: 100,
+                                  height: 30,
+                                  label: 'Sign Out',
+                                  color: accentGrey,
+                                  textColor: Colors.black,
+                                  isLoading: false,
+                                  onTap: () {},
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SliverGrid(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      mainAxisSpacing: 1.0,
+                      crossAxisSpacing: 1.0,
+                      crossAxisCount: 3,
+                    ),
+                    delegate: SliverChildBuilderDelegate(
+                      (BuildContext context, int index) {
+                        final post = postSnapshot.data!.docs[index];
+                        return GestureDetector(
+                          onTap: () {},
+                          child: Image.network(
+                            post['postUrl'],
+                            fit: BoxFit.cover,
+                          ),
+                        );
+                      },
+                      childCount: postSnapshot.data!.docs.length,
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+      },
     );
   }
 }
